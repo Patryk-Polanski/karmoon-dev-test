cc95917291d948e036a24330fef97a8e11c30e6e
Patryk <patryk@selected.org>
2022-03-24 17:39:36 +0000

    restructure project cards behaviour, add JS

--- dist/index.26f645e3.js
+++ dist/index.26f645e3.js
@@ -1,13 +1,12 @@
 'use strict';
 // ZONE - js classes
 const headerSquashedClass = 's-header--squashed';
+const cardsWrapperLeftClass = 's-projects__cards-wrapper--shift-left-js';
+const cardsWrapperRightClass = 's-projects__cards-wrapper--shift-right-js';
 // ZONE - toggle header states
 const initHeaderObserverTop = function(section, headerEl) {
     const headerExtend = function(entries) {
-        if (entries[0].isIntersecting) {
-            console.log('remove the class');
-            headerEl.classList.remove(headerSquashedClass);
-        }
+        if (entries[0].isIntersecting) headerEl.classList.remove(headerSquashedClass);
     };
     const bannerObserver = new IntersectionObserver(headerExtend, {
         root: null,
@@ -17,10 +16,7 @@ const initHeaderObserverTop = function(section, headerEl) {
 };
 const initHeaderObserverBottom = function(section, headerEl) {
     const headerSquash = function(entries) {
-        if (entries[0].isIntersecting) {
-            console.log('add the class');
-            headerEl.classList.add(headerSquashedClass);
-        }
+        if (entries[0].isIntersecting) headerEl.classList.add(headerSquashedClass);
     };
     const bannerObserver = new IntersectionObserver(headerSquash, {
         root: null,
@@ -28,11 +24,31 @@ const initHeaderObserverBottom = function(section, headerEl) {
     });
     bannerObserver.observe(section);
 };
+// ZONE - Project gallery
+const displayProjectCard = function(target, cardPosition, cardsWrapper) {
+    const cardWidth = target.getBoundingClientRect().width * 0.7;
+    if (cardPosition === 'left') cardsWrapper.style.transform = 'translate(' + cardWidth + 'px, 0)';
+    if (cardPosition === 'center') cardsWrapper.style.transform = 'translate(0, 0)';
+    if (cardPosition === 'right') cardsWrapper.style.transform = 'translate(-' + cardWidth + 'px, 0)';
+};
 // ZONE - DOM Content Loaded
 window.addEventListener('DOMContentLoaded', ()=>{
     const headerEl = document.querySelector('.js-header');
     const headerObserverTopEl = document.querySelector('.js-header-helper-top');
     const headerObserverBottomEl = document.querySelector('.js-header-helper-bottom');
+    const projectCardLeftEl = document.querySelector('.js-project-card-left');
+    const projectCardCenterEl = document.querySelector('.js-project-card-center');
+    const projectCardRightEl = document.querySelector('.js-project-card-right');
+    const projectCardsWrapperEl = document.querySelector('.js-projects-cards-wrapper');
+    projectCardLeftEl.addEventListener('mouseenter', (e)=>{
+        displayProjectCard(e.target, 'left', projectCardsWrapperEl);
+    });
+    projectCardCenterEl.addEventListener('mouseenter', (e)=>{
+        displayProjectCard(e.target, 'center', projectCardsWrapperEl);
+    });
+    projectCardRightEl.addEventListener('mouseenter', (e)=>{
+        displayProjectCard(e.target, 'right', projectCardsWrapperEl);
+    });
     initHeaderObserverTop(headerObserverTopEl, headerEl);
     initHeaderObserverBottom(headerObserverBottomEl, headerEl);
 });
--- src/js/script.js
+++ src/js/script.js
@@ -2,13 +2,14 @@
 
 // ZONE - js classes
 const headerSquashedClass = 's-header--squashed';
+const cardsWrapperLeftClass = 's-projects__cards-wrapper--shift-left-js';
+const cardsWrapperRightClass = 's-projects__cards-wrapper--shift-right-js';
 
 // ZONE - toggle header states
 
 const initHeaderObserverTop = function (section, headerEl) {
     const headerExtend = function (entries) {
         if (entries[0].isIntersecting) {
-            console.log('remove the class');
             headerEl.classList.remove(headerSquashedClass);
         }
     };
@@ -24,7 +25,6 @@ const initHeaderObserverTop = function (section, headerEl) {
 const initHeaderObserverBottom = function (section, headerEl) {
     const headerSquash = function (entries) {
         if (entries[0].isIntersecting) {
-            console.log('add the class');
             headerEl.classList.add(headerSquashedClass);
         }
     };
@@ -37,6 +37,22 @@ const initHeaderObserverBottom = function (section, headerEl) {
     bannerObserver.observe(section);
 };
 
+// ZONE - Project gallery
+
+const displayProjectCard = function (target, cardPosition, cardsWrapper) {
+    const cardWidth = target.getBoundingClientRect().width * 0.7;
+    if (cardPosition === 'left') {
+        cardsWrapper.style.transform = 'translate(' + cardWidth + 'px, 0)';
+    }
+
+    if (cardPosition === 'center') {
+        cardsWrapper.style.transform = 'translate(0, 0)';
+    }
+
+    if (cardPosition === 'right')
+        cardsWrapper.style.transform = 'translate(-' + cardWidth + 'px, 0)';
+};
+
 // ZONE - DOM Content Loaded
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -45,6 +61,26 @@ window.addEventListener('DOMContentLoaded', () => {
     const headerObserverBottomEl = document.querySelector(
         '.js-header-helper-bottom'
     );
+    const projectCardLeftEl = document.querySelector('.js-project-card-left');
+    const projectCardCenterEl = document.querySelector(
+        '.js-project-card-center'
+    );
+    const projectCardRightEl = document.querySelector('.js-project-card-right');
+    const projectCardsWrapperEl = document.querySelector(
+        '.js-projects-cards-wrapper'
+    );
+
+    projectCardLeftEl.addEventListener('mouseenter', (e) => {
+        displayProjectCard(e.target, 'left', projectCardsWrapperEl);
+    });
+
+    projectCardCenterEl.addEventListener('mouseenter', (e) => {
+        displayProjectCard(e.target, 'center', projectCardsWrapperEl);
+    });
+
+    projectCardRightEl.addEventListener('mouseenter', (e) => {
+        displayProjectCard(e.target, 'right', projectCardsWrapperEl);
+    });
 
     initHeaderObserverTop(headerObserverTopEl, headerEl);
     initHeaderObserverBottom(headerObserverBottomEl, headerEl);
